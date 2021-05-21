93855046767dc6cea1ed07f022b5061021597da5
Lucira Silva <luciracs0@gmail.com>
2021-05-21 14:02:59 -0300

    style: global style done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./styles/global";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
