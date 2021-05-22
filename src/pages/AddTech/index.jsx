e177e0839d2cfbc231840453d6fa94415cbda0f0
Lucira Silva <luciracs0@gmail.com>
2021-05-22 12:04:29 -0300

    feat: delete techs added

--- src/pages/AddTech/index.jsx
+++ src/pages/AddTech/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Button from "../../components/Button";
 import api from "../../services";
 import { useHistory } from "react-router";
+import { toast } from "react-toastify";
 
 const AddTech = ({ isAuthorized }) => {
   const history = useHistory();
@@ -12,7 +13,7 @@ const AddTech = ({ isAuthorized }) => {
 
   const [people, setPeople] = useState([]);
 
-  const [error, setError] = useState("");
+  const [wannaAdd, setWannaAdd] = useState(true);
 
   const [token] = useState(
     JSON.parse(localStorage.getItem("@Kenziehub:token")) || ""
@@ -35,75 +36,120 @@ const AddTech = ({ isAuthorized }) => {
       const { id } = hasTech.techs.filter(
         (desiredTech) => desiredTech.title === title
       )[0];
-      api.put(
-        `/users/techs/${id}`,
-        {
-          status,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-    }
-  };
-  useEffect(() => {
-    if (people.length !== 0) {
-      handleUpdateTech();
-    }
-  }, [people]);
-  const handleOnSubmit = () => {
-    if (title !== "" && status !== "") {
       api
-        .post(
-          "/users/techs",
-          { title, status },
+        .put(
+          `/users/techs/${id}`,
+          {
+            status,
+          },
           {
             headers: {
               Authorization: `Bearer ${token}`,
             },
           }
         )
-        .catch((error) => {
-          setError(
-            "Essa tecnologia já existe no seu perfil, atualize seu status"
-          );
-          getTech();
-        });
+        .then((_) =>
+          toast.success("Tecnologia já existente, atualização de status feita")
+        )
+        .catch((_) => toast.error("Erro ao atualizar"));
     }
   };
+  const handleDeleteTech = () => {
+    const hasTech = people.find((pessoa) => pessoa.id === userId);
+    if (hasTech) {
+      const { id } = hasTech.techs.filter(
+        (desiredTech) => desiredTech.title === title
+      )[0];
+      api
+        .delete(`/users/techs/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((_) => toast.success("Tecnologia deletada com sucesso"))
+        .catch((_) => toast.error("Erro em deletar tecnologia"));
+    }
+  };
+  useEffect(() => {
+    if (people.length !== 0) {
+      if (wannaAdd) {
+        handleUpdateTech();
+      } else {
+        handleDeleteTech();
+      }
+    }
+  }, [people]);
+
+  const handlePost = () => {
+    api
+      .post(
+        "/users/techs",
+        { title, status },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((_) => toast.success("Tecnologia adicionada com sucesso"))
+      .catch((_) => {
+        toast.error("Falha ao adicionar tecnologia");
+        getTech();
+      });
+  };
+  const handleOnSubmit = () => {
+    if (title !== "" && status !== "" && wannaAdd) {
+      handlePost();
+    } else if (!wannaAdd) {
+      getTech();
+    }
+  };
+  const getDatas = () => {
+      api.get(`/users/${userId}`).then((response) => history.push("/profiledata", response.data));
+  }
   return (
     <div>
-      <label htmlFor="name">Nome</label>
-      <input
-        id="name"
-        type="text"
-        placeholder="Tecnologia"
-        onChange={(e) => setTitle(e.target.value)}
-      />
+      {wannaAdd ? (
+        <div>
+          <label htmlFor="title">Nome</label>
+          <input
+            id="title"
+            type="text"
+            placeholder="Tecnologia"
+            onChange={(e) => setTitle(e.target.value)}
+          />
 
-      <label htmlFor="status">status</label>
-      <select onChange={(e) => setStatus(e.target.value)} id="status" >
-        <option value="iniciante" >iniciante</option>
-        <option value="intermediário">intermediário</option>
-        <option value="avançado">avançado</option>
-      </select>
+          <label htmlFor="status">status</label>
+          <select onChange={(e) => setStatus(e.target.value)} id="status">
+            <option value="iniciante">iniciante</option>
+            <option value="intermediário">intermediário</option>
+            <option value="avançado">avançado</option>
+          </select>
 
-      <Button handleClick={handleOnSubmit}>
-        {!error ? "Adicionar" : "Atualizar status"}
-      </Button>
-      <Button
-        handleClick={() => {
-          api
-            .get(`/users/${userId}`)
-            .then((response) => history.push("/profiledata", response.data));
-        }}
-      >
-        Ver perfil
-      </Button>
+          <Button handleClick={handleOnSubmit}>Adicionar</Button>
+          <Button handleClick={getDatas}>Ver perfil</Button>
 
-      {error && <span>{error}</span>}
+          <Button handleClick={() => setWannaAdd(false)}>
+            Deletar tecnologias
+          </Button>
+        </div>
+      ) : (
+        <div>
+          <label htmlFor="title">Nome</label>
+          <input
+            id="title"
+            type="text"
+            placeholder="Tecnologia"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Button handleClick={handleOnSubmit}>Deletar</Button>
+          <Button handleClick={getDatas}>Ver perfil</Button>
+
+          <Button handleClick={() => setWannaAdd(true)}>
+            Adicionar/atualizar tecnologias
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
